49c245da8aa5e96fb2faf6a6639021cc3b7783f8
zizy <mezizy@live.com>
2017-10-22 22:58:27 +0800

    no msg

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueProgressiveImage from 'vue-progressive-image'
+
+Vue.use(VueProgressiveImage)
 
 Vue.config.productionTip = false
 
