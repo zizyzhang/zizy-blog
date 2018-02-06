696af9500419eb77682edc4994c996fd6e6c1657
zizy <mezizy@live.com>
2018-02-06 20:35:47 +0800

    update

--- debug/import-dayone.js
+++ debug/import-dayone.js
@@ -13,39 +13,40 @@ let dayOne ={
       "location" : {
         "region" : {
           "center" : {
-            "longitude" : 114.13536071777344,
-            "latitude" : 22.283184051513672
+            "longitude" : 114.136474609375,
+            "latitude" : 22.282928466796875
           },
           "radius" : 75
         },
         "localityName" : "Hong Kong",
         "country" : "Hong Kong",
-        "longitude" : 114.13536071777344,
+        "longitude" : 114.136474609375,
         "administrativeArea" : "Hong Kong",
         "placeName" : "The University of Hong Kong",
-        "latitude" : 22.283184051513672
+        "latitude" : 22.282928466796875
       },
-      "sourceString" : "visit-49B0A6C8-01BA-4DC4-B7D8-3EEEF7DBCDF5",
+      "sourceString" : "visit-7A1927CF-39EC-4250-8AED-D480C9CC26D0",
       "userActivity" : {
         "activityName" : "Stationary"
       },
-      "text" : "早起上课下午睡觉晚上聚餐的一天\n\n![](dayone-moment:\/\/A9ED7B76BC164E3D946D3A03C367D014)\n\n心情3分。回到学校的第一次聚餐。\n早起赶回学校上课，ai的课比想象中的偏理论。\n晚上聚餐吃韩料，总觉得还没有那天深圳路边餐厅的正宗一些……\n说不上是开心还是不开心。好像一切回到了正常的轨迹，又似乎哪里有什么不对。说不清…",
+      "text" : "上课被公安局查水表的一天\n\n![](dayone-moment:\/\/A4EE1B8F13F642099FA54BE2B562CCA4)\n\n心情3分。事情真是接连不断。遭遇查水表也是意想不到。还好一切都还正常。\n最近闲下来只想好好休息买买东西打打游戏。\n无心学习也是可怕。忙碌的生活。之后要好好学习了！",
       "weather" : {
-        "temperatureCelsius" : 18,
-        "weatherServiceName" : "Forecast.io",
-        "windBearing" : 89,
-        "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Partly Cloudy",
-        "pressureMB" : 1017.0399780273438,
-        "visibilityKM" : 10.010000228881836,
-        "relativeHumidity" : 89,
-        "windSpeedKPH" : 14.979999542236328,
-        "weatherCode" : "cloudy-night",
-        "sunsetDate" : "1970-01-01T00:00:00Z"
+        "sunsetDate" : "2018-01-31T10:12:09Z",
+        "temperatureCelsius" : 9,
+        "weatherServiceName" : "HAMweather",
+        "windBearing" : 50,
+        "sunriseDate" : "2018-01-30T23:03:19Z",
+        "conditionsDescription" : "Light Rain",
+        "pressureMB" : 1021,
+        "visibilityKM" : 6,
+        "relativeHumidity" : 94,
+        "windSpeedKPH" : 11,
+        "weatherCode" : "rain-night",
+        "windChillCelsius" : 7
       },
-      "creationDate" : "2018-01-19T16:24:02Z",
+      "creationDate" : "2018-01-30T16:22:17Z",
       "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "744DAD246447416D8C34083376BCAE4F",
+      "uuid" : "F8616B6A8BFA44DA8C6BB7FA16E300AB",
       "duration" : 0,
       "photos" : [
         {
@@ -53,27 +54,27 @@ let dayOne ={
           "orderInEntry" : 0,
           "width" : 2048,
           "type" : "jpeg",
-          "identifier" : "A9ED7B76BC164E3D946D3A03C367D014",
-          "date" : "2018-01-20T16:24:02Z",
+          "identifier" : "A4EE1B8F13F642099FA54BE2B562CCA4",
+          "date" : "2018-01-31T16:22:17Z",
           "location" : {
             "region" : {
               "center" : {
-                "longitude" : 114.13536071777344,
-                "latitude" : 22.283184048597214
+                "longitude" : 114.136474609375,
+                "latitude" : 22.282928463880435
               },
-              "identifier" : "<+22.28318405,+114.13536072> radius 141.83",
-              "radius" : 141.83262274255571
+              "identifier" : "<+22.28292846,+114.13647461> radius 141.83",
+              "radius" : 141.83262421846575
             },
             "localityName" : "Hong Kong",
             "country" : "Hong Kong",
             "timeZoneName" : "Asia\/Hong_Kong",
             "administrativeArea" : "Hong Kong",
-            "longitude" : 114.13536071777344,
+            "longitude" : 114.136474609375,
             "placeName" : "The University of Hong Kong",
-            "latitude" : 22.283184051513672
+            "latitude" : 22.282928466796875
           },
           "height" : 2048,
-          "md5" : "1ba46e963224a6135e46e020f8ebc97c",
+          "md5" : "e03a5b310aa09d2af9435624656b82a5",
           "focalLength" : "(null)"
         }
       ]
@@ -83,180 +84,39 @@ let dayOne ={
       "location" : {
         "region" : {
           "center" : {
-            "longitude" : 114.13594818115234,
-            "latitude" : 22.286855697631836
+            "longitude" : 114.13737487792969,
+            "latitude" : 22.285306930541992
           },
           "radius" : 75
         },
         "localityName" : "Hong Kong",
         "country" : "Hong Kong",
-        "longitude" : 114.13594818115234,
-        "administrativeArea" : "Hong Kong",
-        "placeName" : "363–375 Des Voeux Road West, Hong Kong",
-        "latitude" : 22.286855697631836
-      },
-      "sourceString" : "visit-5ADF781A-0AAA-49F3-8FDB-C4899D294534",
-      "userActivity" : {
-        "activityName" : "Stationary"
-      },
-      "text" : "逛商场各种买买买约拉面的一天\n\n![](dayone-moment:\/\/361ED60A173D4CD3B45DAC967D13CBB8)\n\n心情3分。\n突然又回到一个人找吃找喝的时光。\n逛商场但是逛的有些疲乏，感觉一眼望去的东西还是类似。\n晚上约拉面还是挺好吃的！",
-      "weather" : {
-        "temperatureCelsius" : 18,
-        "weatherServiceName" : "Forecast.io",
-        "windBearing" : 80,
-        "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Partly Cloudy",
-        "pressureMB" : 1015.9600219726562,
-        "visibilityKM" : 10.010000228881836,
-        "relativeHumidity" : 84,
-        "windSpeedKPH" : 15.579999923706055,
-        "weatherCode" : "cloudy-night",
-        "sunsetDate" : "1970-01-01T00:00:00Z"
-      },
-      "creationDate" : "2018-01-20T17:30:31Z",
-      "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "0DCF2BDD2F5D42F38CD8C8971D209644",
-      "duration" : 0,
-      "photos" : [
-        {
-          "fnumber" : "(null)",
-          "orderInEntry" : 0,
-          "width" : 2048,
-          "type" : "jpeg",
-          "identifier" : "361ED60A173D4CD3B45DAC967D13CBB8",
-          "date" : "2018-01-23T17:30:31Z",
-          "location" : {
-            "region" : {
-              "center" : {
-                "longitude" : 114.13594818115243,
-                "latitude" : 22.286855694714845
-              },
-              "identifier" : "<+22.28685569,+114.13594818> radius 141.83",
-              "radius" : 141.83260151440376
-            },
-            "localityName" : "Hong Kong",
-            "country" : "Hong Kong",
-            "timeZoneName" : "Asia\/Hong_Kong",
-            "administrativeArea" : "Hong Kong",
-            "longitude" : 114.13594818115234,
-            "placeName" : "363–375 Des Voeux Road West, Hong Kong",
-            "latitude" : 22.286855697631836
-          },
-          "height" : 2048,
-          "md5" : "eed59402cc1b2ca400759a6e1f0084db",
-          "focalLength" : "(null)"
-        }
-      ]
-    },
-    {
-      "starred" : false,
-      "location" : {
-        "region" : {
-          "center" : {
-            "longitude" : 114.13727569580078,
-            "latitude" : 22.285621643066406
-          },
-          "radius" : 75
-        },
-        "localityName" : "Hong Kong",
-        "country" : "Hong Kong",
-        "longitude" : 114.13727569580078,
+        "longitude" : 114.13737487792969,
         "administrativeArea" : "Hong Kong",
         "placeName" : "179 Third Street, Hong Kong",
-        "latitude" : 22.285621643066406
+        "latitude" : 22.285306930541992
       },
-      "sourceString" : "visit-5ADF781A-0AAA-49F3-8FDB-C4899D294534",
-      "userActivity" : {
-        "activityName" : "Stationary"
-      },
-      "text" : "叔叔不约再次爆炸的一天\n\n![](dayone-moment:\/\/016820B6F26742DDB61731A522500C66)\n\n心情4分。本次爆炸迎来了历史上的第三次人数增加，如果预期没错的话会成长到原来的1.5倍吧。与此同时应该有新的商业模式可以呼之欲出啦。晚上很忙还是要运动一下的！",
-      "weather" : {
-        "temperatureCelsius" : 17,
-        "weatherServiceName" : "Forecast.io",
-        "windBearing" : 46,
-        "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Mostly Cloudy",
-        "pressureMB" : 1014.4299926757812,
-        "visibilityKM" : 5.8600001335144043,
-        "relativeHumidity" : 85,
-        "windSpeedKPH" : 5.070000171661377,
-        "weatherCode" : "cloudy-night",
-        "sunsetDate" : "1970-01-01T00:00:00Z"
-      },
-      "creationDate" : "2018-01-21T17:31:27Z",
-      "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "2E0DD29F16A64D0AA419F2FA4B084BA7",
-      "duration" : 0,
-      "photos" : [
-        {
-          "fnumber" : "(null)",
-          "orderInEntry" : 0,
-          "width" : 2048,
-          "type" : "jpeg",
-          "identifier" : "016820B6F26742DDB61731A522500C66",
-          "date" : "2018-01-23T17:31:27Z",
-          "location" : {
-            "region" : {
-              "center" : {
-                "longitude" : 114.13727569580078,
-                "latitude" : 22.28562164233719
-              },
-              "identifier" : "<+22.28562164,+114.13727570> radius 70.92",
-              "radius" : 70.916248044224247
-            },
-            "localityName" : "Hong Kong",
-            "country" : "Hong Kong",
-            "timeZoneName" : "Asia\/Hong_Kong",
-            "administrativeArea" : "Hong Kong",
-            "longitude" : 114.13727569580078,
-            "placeName" : "179 Third Street, Hong Kong",
-            "latitude" : 22.285621643066406
-          },
-          "height" : 2048,
-          "md5" : "ac9e62485d9388cb7b2bfaf7ea292b7e",
-          "focalLength" : "(null)"
-        }
-      ]
-    },
-    {
-      "starred" : false,
-      "location" : {
-        "region" : {
-          "center" : {
-            "longitude" : 114.13666534423828,
-            "latitude" : 22.28631591796875
-          },
-          "radius" : 75
-        },
-        "localityName" : "Hong Kong",
-        "country" : "Hong Kong",
-        "longitude" : 114.13666534423828,
-        "administrativeArea" : "Hong Kong",
-        "placeName" : "11–19 Whitty Street, Hong Kong",
-        "latitude" : 22.28631591796875
-      },
-      "sourceString" : "visit-5ADF781A-0AAA-49F3-8FDB-C4899D294534",
+      "creationDate" : "2018-02-03T16:44:02Z",
       "userActivity" : {
         "activityName" : "Stationary",
-        "stepCount" : 5
+        "stepCount" : 9
       },
-      "text" : "一整天忙叔叔不约觉得真的忙不过来的一天\n\n![](dayone-moment:\/\/808759E7785F4C7DAEF22423D007BC48)\n\n心情4分。人数每次增加总是会有新的idea和新的技术架构。虽然忙但是很开心的感觉。\n人生理想应该在此时短暂的又实现了一些吧。接下来要面对的可能是更激烈的竞争了。谁知道呢。\n果然是一旦开始，就很难再停下来了啊。",
+      "text" : "逐渐作息恢复正常周末拔牙的一周\n\n![](dayone-moment:\/\/3F3D4435E0714813BC4D0AF89F3BB251)\n\n心情3.5分。逐渐稳定下来的第一周，开始关注让生活提高一些些水准的方法。看了很多年薪**的人是怎么生活的诸如此类，觉得自己更应该多开阔眼界，投资自己总是最好的。每个人还是要有自己的生活方式才对。\n尝试面试招聘，但还是觉得有太多东西没有考虑周全，还不可以轻易行动。把叔叔不约申请了商标，顺利的话应该又排除一些后患。\n尝试了一些新餐厅，有一些还不错。投资了一直想要做的低估值指数。\n周末是深圳拔牙，这种疼痛程度真是好久未经历了…辗转买了暖风机之后，终于不会晚上冻醒。\n总的来说，生活慢慢走向正常轨迹，希望一直没有意外。",
       "weather" : {
-        "temperatureCelsius" : 18,
+        "temperatureCelsius" : 10,
         "weatherServiceName" : "Forecast.io",
-        "windBearing" : 71,
+        "windBearing" : 34,
         "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Partly Cloudy",
-        "pressureMB" : 1015.1400146484375,
-        "visibilityKM" : 5.9099998474121094,
-        "relativeHumidity" : 84,
-        "windSpeedKPH" : 10.430000305175781,
-        "weatherCode" : "cloudy-night",
+        "conditionsDescription" : "Breezy and Partly Cloudy",
+        "pressureMB" : 1025.530029296875,
+        "visibilityKM" : 10.010000228881836,
+        "relativeHumidity" : 51,
+        "windSpeedKPH" : 24.479999542236328,
+        "weatherCode" : "wind",
         "sunsetDate" : "1970-01-01T00:00:00Z"
       },
-      "creationDate" : "2018-01-22T17:32:06Z",
       "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "9CF756D7557E4BD38760C021A72242CB",
+      "uuid" : "3CB0B5DE24564EF4818944AD1C8BBDEB",
       "duration" : 0,
       "photos" : [
         {
@@ -264,98 +124,9 @@ let dayOne ={
           "orderInEntry" : 0,
           "width" : 2048,
           "type" : "jpeg",
-          "identifier" : "808759E7785F4C7DAEF22423D007BC48",
-          "date" : "2018-01-23T17:32:06Z",
-          "location" : {
-            "region" : {
-              "center" : {
-                "longitude" : 114.13666534423834,
-                "latitude" : 22.286315915051823
-              },
-              "identifier" : "<+22.28631592,+114.13666534> radius 141.83",
-              "radius" : 141.83260463494406
-            },
-            "localityName" : "Hong Kong",
-            "country" : "Hong Kong",
-            "timeZoneName" : "Asia\/Hong_Kong",
-            "administrativeArea" : "Hong Kong",
-            "longitude" : 114.13666534423828,
-            "placeName" : "11–19 Whitty Street, Hong Kong",
-            "latitude" : 22.28631591796875
-          },
+          "identifier" : "3F3D4435E0714813BC4D0AF89F3BB251",
           "height" : 2048,
-          "md5" : "05d4c5548a8d2f0ec2e656049f609d7e",
-          "focalLength" : "(null)"
-        }
-      ]
-    },
-    {
-      "starred" : false,
-      "location" : {
-        "region" : {
-          "center" : {
-            "longitude" : 114.13626861572266,
-            "latitude" : 22.286369323730469
-          },
-          "radius" : 75
-        },
-        "localityName" : "Hong Kong",
-        "country" : "Hong Kong",
-        "longitude" : 114.13626861572266,
-        "administrativeArea" : "Hong Kong",
-        "placeName" : "406E Des Voeux Road West, Hong Kong",
-        "latitude" : 22.286369323730469
-      },
-      "sourceString" : "visit-F654F4FB-CA42-438F-8887-C8CF6E9C728A",
-      "userActivity" : {
-        "activityName" : "Stationary",
-        "stepCount" : 1811
-      },
-      "text" : "漫长作息破坏完全时间从未如此不够用的一周\n\n![](dayone-moment:\/\/DCDFFA663FCC405784190334435FAEEC)\n\n心情4分。其实已经不知道这个心情到底应该是什么。\n恍惚一周过去，网站人数比一年前已经翻了数百倍。盈利也应该会有百倍地提升了吧。如果没有意外的话。\n生活仿佛和之前还没有太大不同。在香港终于可以不在乎食宿的价格，想要什么不用考虑太多就可以买下。但是除此之外烦心的事情也指数倍的增加。似乎除了盈利以外，多了更多的责任，更多要面对的可能随时发生的意外。\n今天突然发现除了新的备案，APP，商标，运营，运维。一堆一堆的。",
-      "weather" : {
-        "temperatureCelsius" : 9,
-        "weatherServiceName" : "Forecast.io",
-        "windBearing" : 28,
-        "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Mostly Cloudy",
-        "pressureMB" : 1019.0499877929688,
-        "visibilityKM" : 7,
-        "relativeHumidity" : 79,
-        "windSpeedKPH" : 15.739999771118164,
-        "weatherCode" : "partly-cloudy",
-        "sunsetDate" : "1970-01-01T00:00:00Z"
-      },
-      "creationDate" : "2018-01-30T07:58:39Z",
-      "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "DCC79B5569AF458F823FDB540077FA68",
-      "duration" : 0,
-      "photos" : [
-        {
-          "fnumber" : "(null)",
-          "orderInEntry" : 0,
-          "width" : 2048,
-          "type" : "jpeg",
-          "identifier" : "DCDFFA663FCC405784190334435FAEEC",
-          "date" : "2018-01-30T07:58:39Z",
-          "location" : {
-            "region" : {
-              "center" : {
-                "longitude" : 114.13626861572263,
-                "latitude" : 22.28636932300121
-              },
-              "identifier" : "<+22.28636932,+114.13626862> radius 70.92",
-              "radius" : 70.916245880478257
-            },
-            "localityName" : "Hong Kong",
-            "country" : "Hong Kong",
-            "timeZoneName" : "Asia\/Hong_Kong",
-            "administrativeArea" : "Hong Kong",
-            "longitude" : 114.13626861572266,
-            "placeName" : "406E Des Voeux Road West, Hong Kong",
-            "latitude" : 22.286369323730469
-          },
-          "height" : 2048,
-          "md5" : "0fdb932e55497f1222ba1d1fef6d5690",
+          "md5" : "3938a6a6da3589081d922fba032b27e4",
           "focalLength" : "(null)"
         }
       ]
