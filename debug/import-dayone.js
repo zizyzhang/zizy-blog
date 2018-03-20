5517937e1e03b0a0952b2d26e85000faf9268e71
zizy <mezizy@live.com>
2018-03-20 12:09:23 +0800

    update

--- debug/import-dayone.js
+++ debug/import-dayone.js
@@ -13,122 +13,39 @@ let dayOne ={
       "location" : {
         "region" : {
           "center" : {
-            "longitude" : 114.13550567626953,
-            "latitude" : 22.285985946655273
+            "longitude" : 114.13553619384766,
+            "latitude" : 22.28691291809082
           },
           "radius" : 75
         },
         "localityName" : "Hong Kong",
         "country" : "Hong Kong",
-        "longitude" : 114.13550567626953,
+        "longitude" : 114.13553619384766,
         "administrativeArea" : "Hong Kong",
-        "placeName" : "494 Queen's Road West, Hong Kong",
-        "latitude" : 22.285985946655273
+        "placeName" : "Hong Kong Plaza",
+        "latitude" : 22.28691291809082
       },
-      "sourceString" : "visit-C4012681-EA54-4F97-8751-97C56FD7132A",
-      "userActivity" : {
-        "activityName" : "Stationary",
-        "stepCount" : 555
-      },
-      "text" : "告别多事之冬开始有开心的事情发生的四天\n\n![](dayone-moment:\/\/12C7285A51224B59AEA582D824455423)\n\n心情3.5分。\n百度的排名突然间就回来了。\n虽然加了关键词过滤器营业额下降了30%。嗯，好像也算是一件不怎么开心的事情。不知道以后会不会有什么不好的事情发生，人数会不会继续下降。\n但如果达到某个程度的平衡的话，应该也不至于是什么坏事。\n周六考了ai期中考，在某些特殊原因下可能考得还可以。王老板也通过了我改了又改的论文计划。看起来一切都很正常很安全。\n但是周日晚上就比较特别了，和之前那些妖艳的几天都不一样…\n\/\/\/\/和女神逛了海边聊到了四点。真是不知道有什么好说的。感觉真的是只剩下kiss。我们的关系到底进展到了什么样让人匪夷所思的地步自己也说不清。如果这样也不算互相喜欢的话那什么是呢…希望她可以早点分手🙂",
-      "weather" : {
-        "sunsetDate" : "2018-03-04T10:29:13Z",
-        "temperatureCelsius" : 26,
-        "weatherServiceName" : "HAMweather",
-        "windBearing" : 150,
-        "sunriseDate" : "2018-03-03T22:42:55Z",
-        "conditionsDescription" : "Partly Cloudy",
-        "pressureMB" : 1011,
-        "visibilityKM" : 48.280319213867188,
-        "relativeHumidity" : 79,
-        "windSpeedKPH" : 19,
-        "weatherCode" : "partly-cloudy",
-        "windChillCelsius" : 26
-      },
-      "creationDate" : "2018-03-04T04:28:31Z",
-      "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "D9B845D3D79C4752B9B7D9F26EA4348A",
-      "duration" : 0,
-      "photos" : [
-        {
-          "fnumber" : "(null)",
-          "orderInEntry" : 0,
-          "width" : 2048,
-          "type" : "jpeg",
-          "identifier" : "12C7285A51224B59AEA582D824455423",
-          "date" : "2018-03-05T04:28:31Z",
-          "location" : {
-            "region" : {
-              "center" : {
-                "longitude" : 114.13550567626953,
-                "latitude" : 22.285985945926043
-              },
-              "identifier" : "<+22.28598595,+114.13550568> radius 70.92",
-              "radius" : 70.916246988569824
-            },
-            "localityName" : "Hong Kong",
-            "country" : "Hong Kong",
-            "timeZoneName" : "Asia\/Hong_Kong",
-            "administrativeArea" : "Hong Kong",
-            "longitude" : 114.13550567626953,
-            "placeName" : "494 Queen's Road West, Hong Kong",
-            "latitude" : 22.285985946655273
-          },
-          "weather" : {
-            "weatherServiceName" : "Forecast.io",
-            "windBearing" : 164,
-            "conditionsDescription" : "Partly Cloudy",
-            "pressureMB" : 1013.280029296875,
-            "visibilityKM" : 10.010000228881836,
-            "relativeHumidity" : 73,
-            "windSpeedKPH" : 2.6099998950958252,
-            "weatherCode" : "partly-cloudy",
-            "temperatureCelsius" : 26.520000457763672
-          },
-          "height" : 2048,
-          "md5" : "9064bf827216643657a2947f4f34b5e5",
-          "focalLength" : "(null)"
-        }
-      ]
-    },
-    {
-      "starred" : false,
-      "location" : {
-        "region" : {
-          "center" : {
-            "longitude" : 114.1351318359375,
-            "latitude" : 22.285820007324219
-          },
-          "radius" : 75
-        },
-        "localityName" : "Hong Kong",
-        "country" : "Hong Kong",
-        "longitude" : 114.1351318359375,
-        "administrativeArea" : "Hong Kong",
-        "placeName" : "20 Hill Road, Hong Kong",
-        "latitude" : 22.285820007324219
-      },
-      "sourceString" : "visit-820ED833-135A-4548-904D-89450FDC19F1",
+      "sourceString" : "visit-A28C6D64-8662-4F90-8711-74A0600322EC",
       "userActivity" : {
         "activityName" : "Stationary"
       },
-      "text" : "还算充实的放假三天\n\n![](dayone-moment:\/\/35B557B5DD7043F8AAFCC45042BC1036)\n\n实际上也没有做什么太多事情，就看着时间一点点流去了。周一因为作息混乱睡了半天，周二认识了一下新朋友，看了电影。周三是etho的咖啡厅写游戏，晚上散步的时候遇见传说中的野猪群。",
+      "text" : "做游戏以及第二次喝茶邀约的一周\n\n![](dayone-moment:\/\/D67A059CA7F441429864B6E6F7502595)\n\n总得来说这周从焦虑到慢慢看淡一些虚无的东西。分手之后有一种恢复生活原本模样的感觉。\n前几天沉迷做游戏，对unity慢慢加深理解，似乎真的可以做出来一个什么东西。\n周四的时候开了第一次dissertation会议，有些很厉害的人出现，突然对于自己未来要做的事情有点恐惧。\n叔叔不约处于一个几乎要自己全职投入的状态。学习无法完全的兼顾，只能调低优先级。\n突然觉得应该彻底的把叔叔不约移出中国，但是依然没想好应该怎么做。也不知道所谓彻底到底应该多彻底。\n周五在复习data mining不是很爱这种只为了考试复习的感觉。",
       "weather" : {
-        "temperatureCelsius" : 18,
+        "temperatureCelsius" : 20,
         "weatherServiceName" : "Forecast.io",
-        "windBearing" : 90,
+        "windBearing" : 84,
         "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Breezy and Partly Cloudy",
-        "pressureMB" : 1017.47998046875,
-        "visibilityKM" : 10.010000228881836,
-        "relativeHumidity" : 81,
-        "windSpeedKPH" : 32.569999694824219,
-        "weatherCode" : "wind",
+        "conditionsDescription" : "Clear",
+        "pressureMB" : 1015.9600219726562,
+        "visibilityKM" : 9,
+        "relativeHumidity" : 90,
+        "windSpeedKPH" : 18.329999923706055,
+        "weatherCode" : "clear-night",
         "sunsetDate" : "1970-01-01T00:00:00Z"
       },
-      "creationDate" : "2018-03-06T17:52:16Z",
+      "creationDate" : "2018-03-16T17:13:32Z",
       "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "1748D28E21BD43F3A96E958B906D97EF",
+      "uuid" : "33F6158FF03F4109BA9D1ED7FB7337D6",
       "duration" : 0,
       "photos" : [
         {
@@ -136,38 +53,38 @@ let dayOne ={
           "orderInEntry" : 0,
           "width" : 2048,
           "type" : "jpeg",
-          "identifier" : "35B557B5DD7043F8AAFCC45042BC1036",
-          "date" : "2018-03-07T17:52:16Z",
+          "identifier" : "D67A059CA7F441429864B6E6F7502595",
+          "date" : "2018-03-16T17:13:32Z",
           "location" : {
             "region" : {
               "center" : {
-                "longitude" : 114.13513183593747,
-                "latitude" : 22.285820006595003
+                "longitude" : 114.13553619384766,
+                "latitude" : 22.286912917361569
               },
-              "identifier" : "<+22.28582001,+114.13513184> radius 70.92",
-              "radius" : 70.916247471476311
+              "identifier" : "<+22.28691292,+114.13553619> radius 70.92",
+              "radius" : 70.916244306138481
             },
             "localityName" : "Hong Kong",
             "country" : "Hong Kong",
             "timeZoneName" : "Asia\/Hong_Kong",
             "administrativeArea" : "Hong Kong",
-            "longitude" : 114.1351318359375,
-            "placeName" : "20 Hill Road, Hong Kong",
-            "latitude" : 22.285820007324219
+            "longitude" : 114.13553619384766,
+            "placeName" : "Hong Kong Plaza",
+            "latitude" : 22.28691291809082
           },
           "weather" : {
             "weatherServiceName" : "Forecast.io",
-            "windBearing" : 356,
+            "windBearing" : 84,
             "conditionsDescription" : "Clear",
-            "pressureMB" : 1017.3699951171875,
-            "visibilityKM" : 3.7000000476837158,
+            "pressureMB" : 1015.9600219726562,
+            "visibilityKM" : 9,
             "relativeHumidity" : 90,
-            "windSpeedKPH" : 23.319999694824219,
+            "windSpeedKPH" : 18.329999923706055,
             "weatherCode" : "clear-night",
-            "temperatureCelsius" : 18.069999694824219
+            "temperatureCelsius" : 20.850000381469727
           },
           "height" : 2048,
-          "md5" : "a7ed86e67955505026a26b82f4690239",
+          "md5" : "676e45ac1df83e1134f9c5a83dc13d14",
           "focalLength" : "(null)"
         }
       ]
@@ -177,40 +94,39 @@ let dayOne ={
       "location" : {
         "region" : {
           "center" : {
-            "longitude" : 114.14292907714844,
-            "latitude" : 22.286857604980469
+            "longitude" : 114.13798522949219,
+            "latitude" : 22.285934448242188
           },
           "radius" : 75
         },
         "localityName" : "Hong Kong",
         "country" : "Hong Kong",
-        "longitude" : 114.14292907714844,
+        "longitude" : 114.13798522949219,
         "administrativeArea" : "Hong Kong",
-        "placeName" : "260 Queen's Road West, Hong Kong",
-        "latitude" : 22.286857604980469
+        "placeName" : "179 Third Street, Hong Kong",
+        "latitude" : 22.285934448242188
       },
-      "sourceString" : "visit-54DE65C5-BC23-4BA6-A53E-70BEDB433CD7",
+      "creationDate" : "2018-03-19T01:26:08Z",
       "userActivity" : {
-        "activityName" : "Stationary",
-        "stepCount" : 1184
+        "activityName" : "Walking",
+        "stepCount" : 2004
       },
-      "text" : "剩下的一半假期\n\n![](dayone-moment:\/\/A424DCAED41F47909F7F11A1DE29B0B2)\n\n广州二日游，叔叔不约流量一直下滑，然后到一个慢慢看淡随他去的状态。\n收入非常不平衡，可能有上升空间或许没有。\n加了过滤器感觉一切都重新开始了阿。\n贴吧改了规则变得更难顶帖。\n感情很糟糕，工作很糟糕，学习也有点糟糕。",
+      "text" : "复习DM沉迷Linux的一周\n\n![](dayone-moment:\/\/EE147ADB2C134FC2951A2650DA02AD03)\n\n周五没有存在感的放假，复习了一天的datamining，周六没做什么，周日是对网站的修修改改，新的架构，一切似乎看起来更有道理了一点。",
       "weather" : {
-        "temperatureCelsius" : 20,
+        "temperatureCelsius" : 22,
         "weatherServiceName" : "Forecast.io",
-        "windBearing" : 70,
+        "windBearing" : 86,
         "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Clear",
-        "pressureMB" : 1020.5499877929688,
-        "visibilityKM" : 10.010000228881836,
-        "relativeHumidity" : 68,
-        "windSpeedKPH" : 16.399999618530273,
-        "weatherCode" : "clear",
+        "conditionsDescription" : "Partly Cloudy",
+        "pressureMB" : 1013.8200073242188,
+        "visibilityKM" : 7,
+        "relativeHumidity" : 87,
+        "windSpeedKPH" : 13.779999732971191,
+        "weatherCode" : "partly-cloudy",
         "sunsetDate" : "1970-01-01T00:00:00Z"
       },
-      "creationDate" : "2018-03-12T01:45:29Z",
       "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "B2F71AB394374288BB40608D1D65B2EF",
+      "uuid" : "0FEC207FED1D48D6BBD97631BF7E9150",
       "duration" : 0,
       "photos" : [
         {
@@ -218,38 +134,9 @@ let dayOne ={
           "orderInEntry" : 0,
           "width" : 2048,
           "type" : "jpeg",
-          "identifier" : "A424DCAED41F47909F7F11A1DE29B0B2",
-          "date" : "2018-03-13T01:45:29Z",
-          "location" : {
-            "region" : {
-              "center" : {
-                "longitude" : 114.14292907714844,
-                "latitude" : 22.286857604251217
-              },
-              "identifier" : "<+22.28685760,+114.14292908> radius 70.92",
-              "radius" : 70.916244469128387
-            },
-            "localityName" : "Hong Kong",
-            "country" : "Hong Kong",
-            "timeZoneName" : "Asia\/Hong_Kong",
-            "administrativeArea" : "Hong Kong",
-            "longitude" : 114.14292907714844,
-            "placeName" : "260 Queen's Road West, Hong Kong",
-            "latitude" : 22.286857604980469
-          },
-          "weather" : {
-            "weatherServiceName" : "Forecast.io",
-            "windBearing" : 77,
-            "conditionsDescription" : "Clear",
-            "pressureMB" : 1018.5399780273438,
-            "visibilityKM" : 10.010000228881836,
-            "relativeHumidity" : 71,
-            "windSpeedKPH" : 13.630000114440918,
-            "weatherCode" : "clear",
-            "temperatureCelsius" : 21.850000381469727
-          },
+          "identifier" : "EE147ADB2C134FC2951A2650DA02AD03",
           "height" : 2048,
-          "md5" : "4ff7e0c6edc826ab187a145036decf93",
+          "md5" : "e39008d88fd2d486bf9e33c7e5c68a96",
           "focalLength" : "(null)"
         }
       ]
