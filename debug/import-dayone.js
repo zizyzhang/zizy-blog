f95284e28b0ae1d62b15414f261a348e4599d171
zizy <mezizy@live.com>
2018-04-05 16:33:23 +0800

    update

--- debug/import-dayone.js
+++ debug/import-dayone.js
@@ -13,39 +13,39 @@ let dayOne ={
       "location" : {
         "region" : {
           "center" : {
-            "longitude" : 114.13553619384766,
-            "latitude" : 22.28691291809082
+            "longitude" : 114.13751220703125,
+            "latitude" : 22.285396575927734
           },
           "radius" : 75
         },
         "localityName" : "Hong Kong",
         "country" : "Hong Kong",
-        "longitude" : 114.13553619384766,
+        "longitude" : 114.13751220703125,
         "administrativeArea" : "Hong Kong",
-        "placeName" : "Hong Kong Plaza",
-        "latitude" : 22.28691291809082
+        "placeName" : "200–208 Third Street, Hong Kong",
+        "latitude" : 22.285396575927734
       },
-      "sourceString" : "visit-A28C6D64-8662-4F90-8711-74A0600322EC",
+      "creationDate" : "2018-03-20T05:32:45Z",
       "userActivity" : {
-        "activityName" : "Stationary"
+        "activityName" : "Stationary",
+        "stepCount" : 2244
       },
-      "text" : "做游戏以及第二次喝茶邀约的一周\n\n![](dayone-moment:\/\/D67A059CA7F441429864B6E6F7502595)\n\n总得来说这周从焦虑到慢慢看淡一些虚无的东西。分手之后有一种恢复生活原本模样的感觉。\n前几天沉迷做游戏，对unity慢慢加深理解，似乎真的可以做出来一个什么东西。\n周四的时候开了第一次dissertation会议，有些很厉害的人出现，突然对于自己未来要做的事情有点恐惧。\n叔叔不约处于一个几乎要自己全职投入的状态。学习无法完全的兼顾，只能调低优先级。\n突然觉得应该彻底的把叔叔不约移出中国，但是依然没想好应该怎么做。也不知道所谓彻底到底应该多彻底。\n周五在复习data mining不是很爱这种只为了考试复习的感觉。",
+      "text" : "深圳半日游深夜思考人生过多的一天\n\n![](dayone-moment:\/\/775523760E3F4CD3BBE190CE4D072A29)\n\n一个奇妙的人生转折点。早上考了一个大概最糟糕的考试。然后就吃完饭深圳喝茶电影。晚上回来的时候就顺便海边思考了一下过往人生。由于某些不可描述的原因，讲了很多心里不想说的话，知道了很多悲伤的事实。\n人生还是要继续过，game的作业也要继续写，做不到可以坦然接受。但是无论如何也要继续好好过下去。但也不是什么最糟糕的事情吧。",
       "weather" : {
-        "temperatureCelsius" : 20,
+        "temperatureCelsius" : 22,
         "weatherServiceName" : "Forecast.io",
-        "windBearing" : 84,
+        "windBearing" : 346,
         "sunriseDate" : "1970-01-01T00:00:00Z",
-        "conditionsDescription" : "Clear",
-        "pressureMB" : 1015.9600219726562,
-        "visibilityKM" : 9,
-        "relativeHumidity" : 90,
-        "windSpeedKPH" : 18.329999923706055,
-        "weatherCode" : "clear-night",
+        "conditionsDescription" : "Partly Cloudy",
+        "pressureMB" : 1013.469970703125,
+        "visibilityKM" : 10.010000228881836,
+        "relativeHumidity" : 65,
+        "windSpeedKPH" : 23.690000534057617,
+        "weatherCode" : "partly-cloudy",
         "sunsetDate" : "1970-01-01T00:00:00Z"
       },
-      "creationDate" : "2018-03-16T17:13:32Z",
       "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "33F6158FF03F4109BA9D1ED7FB7337D6",
+      "uuid" : "D55C96C9AFD24EC38213487497BC6115",
       "duration" : 0,
       "photos" : [
         {
@@ -53,38 +53,9 @@ let dayOne ={
           "orderInEntry" : 0,
           "width" : 2048,
           "type" : "jpeg",
-          "identifier" : "D67A059CA7F441429864B6E6F7502595",
-          "date" : "2018-03-16T17:13:32Z",
-          "location" : {
-            "region" : {
-              "center" : {
-                "longitude" : 114.13553619384766,
-                "latitude" : 22.286912917361569
-              },
-              "identifier" : "<+22.28691292,+114.13553619> radius 70.92",
-              "radius" : 70.916244306138481
-            },
-            "localityName" : "Hong Kong",
-            "country" : "Hong Kong",
-            "timeZoneName" : "Asia\/Hong_Kong",
-            "administrativeArea" : "Hong Kong",
-            "longitude" : 114.13553619384766,
-            "placeName" : "Hong Kong Plaza",
-            "latitude" : 22.28691291809082
-          },
-          "weather" : {
-            "weatherServiceName" : "Forecast.io",
-            "windBearing" : 84,
-            "conditionsDescription" : "Clear",
-            "pressureMB" : 1015.9600219726562,
-            "visibilityKM" : 9,
-            "relativeHumidity" : 90,
-            "windSpeedKPH" : 18.329999923706055,
-            "weatherCode" : "clear-night",
-            "temperatureCelsius" : 20.850000381469727
-          },
+          "identifier" : "775523760E3F4CD3BBE190CE4D072A29",
           "height" : 2048,
-          "md5" : "676e45ac1df83e1134f9c5a83dc13d14",
+          "md5" : "489a6992a7eaee3a2a4c5d1c9b042dbc",
           "focalLength" : "(null)"
         }
       ]
@@ -94,39 +65,39 @@ let dayOne ={
       "location" : {
         "region" : {
           "center" : {
-            "longitude" : 114.13798522949219,
-            "latitude" : 22.285934448242188
+            "longitude" : 113.31057739257812,
+            "latitude" : 23.106086730957031
           },
           "radius" : 75
         },
-        "localityName" : "Hong Kong",
-        "country" : "Hong Kong",
-        "longitude" : 114.13798522949219,
-        "administrativeArea" : "Hong Kong",
-        "placeName" : "179 Third Street, Hong Kong",
-        "latitude" : 22.285934448242188
+        "localityName" : "Guangzhou",
+        "country" : "China",
+        "longitude" : 113.31057739257812,
+        "administrativeArea" : "Guangdong",
+        "placeName" : "Yuexiu",
+        "latitude" : 23.106086730957031
       },
-      "creationDate" : "2018-03-19T01:26:08Z",
+      "creationDate" : "2018-03-27T09:28:59Z",
       "userActivity" : {
         "activityName" : "Walking",
-        "stepCount" : 2004
+        "stepCount" : 6735
       },
-      "text" : "复习DM沉迷Linux的一周\n\n![](dayone-moment:\/\/EE147ADB2C134FC2951A2650DA02AD03)\n\n周五没有存在感的放假，复习了一天的datamining，周六没做什么，周日是对网站的修修改改，新的架构，一切似乎看起来更有道理了一点。",
+      "text" : "多种极端情绪混合的一星期\n\n![](dayone-moment:\/\/B8249ED877324869BCEA2BE4A3D11A56)\n\n在淘宝买了一个心理咨询。\n然后被弄的心情低落。\n根本就不是什么心理咨询，\n完全就是在调戏我的人生吧……\n还好交了作业后没什么事情。\n可以好好安静思考人生。\n情绪到低谷之后开始反转。\n周四翘了课去深圳默默的思考人生，\n也算是明白了一些什么…\n广州一日游。\n\/\/\/\/周一周二和小姐姐在广州的民宿里愉快玩耍了一天。晚上被多动症小姐姐各种姿势抱着睡了一觉，美好的不是很真实。虽然小姐姐的面试不是很好。开始过上了一天见不到就感觉不完整的生活。",
       "weather" : {
-        "temperatureCelsius" : 22,
+        "temperatureCelsius" : 26,
         "weatherServiceName" : "Forecast.io",
-        "windBearing" : 86,
+        "windBearing" : 285,
         "sunriseDate" : "1970-01-01T00:00:00Z",
         "conditionsDescription" : "Partly Cloudy",
-        "pressureMB" : 1013.8200073242188,
-        "visibilityKM" : 7,
-        "relativeHumidity" : 87,
-        "windSpeedKPH" : 13.779999732971191,
+        "pressureMB" : 1013.47998046875,
+        "visibilityKM" : 10.010000228881836,
+        "relativeHumidity" : 47,
+        "windSpeedKPH" : 3.5399999618530273,
         "weatherCode" : "partly-cloudy",
         "sunsetDate" : "1970-01-01T00:00:00Z"
       },
-      "timeZone" : "Asia\/Hong_Kong",
-      "uuid" : "0FEC207FED1D48D6BBD97631BF7E9150",
+      "timeZone" : "Asia\/Shanghai",
+      "uuid" : "74030E464A0C4D7F80C21B19B64207C4",
       "duration" : 0,
       "photos" : [
         {
@@ -134,9 +105,60 @@ let dayOne ={
           "orderInEntry" : 0,
           "width" : 2048,
           "type" : "jpeg",
-          "identifier" : "EE147ADB2C134FC2951A2650DA02AD03",
+          "identifier" : "B8249ED877324869BCEA2BE4A3D11A56",
           "height" : 2048,
-          "md5" : "e39008d88fd2d486bf9e33c7e5c68a96",
+          "md5" : "ee922ae88e4bd9c3c40f00f6d4057a8c",
+          "focalLength" : "(null)"
+        }
+      ]
+    },
+    {
+      "starred" : false,
+      "location" : {
+        "region" : {
+          "center" : {
+            "longitude" : 114.13790130615234,
+            "latitude" : 22.284671783447266
+          },
+          "radius" : 75
+        },
+        "localityName" : "Hong Kong",
+        "country" : "Hong Kong",
+        "longitude" : 114.13790130615234,
+        "administrativeArea" : "Hong Kong",
+        "placeName" : "The University of Hong Kong",
+        "latitude" : 22.284671783447266
+      },
+      "creationDate" : "2018-04-03T18:19:18Z",
+      "userActivity" : {
+        "activityName" : "Stationary"
+      },
+      "text" : "更新了最想要生活定义的一星期\n\n![](dayone-moment:\/\/2BEE3FA43830410ABE42DBCFCD753BA0)\n\n有一种之前所有想象变成现实的感觉。\ndental学习了几天之后在深圳度过了感觉像是一个世纪的三天。\n虽然是来好好学习的，\n住的loft很精致，\n没怎么玩耍，即便这样还是很充实。\n\/\/\/\/周五的dental不开门，在外面的公园里学习。聊着聊着突然情绪都变的很崩溃。于是和小姐姐坐着丁丁车到了底站又回来。虽然并没有想通什么，只是这样说说内心的想法就似乎可以平静很多。很突然的决定去深圳。周五见面的时候还是做坏事的忐忑。相处一天后就似乎变的很习惯一起生活了。一起去学习，一起回家，一起面对一些琐碎的两个人在一起才会面对的事，一起抱着思考人生，一起睡。感觉根本是自己最希望的生活的样子。不是多么热烈的，似乎甚至有些简单的，和最喜欢的人一起的舒服的生活。",
+      "weather" : {
+        "temperatureCelsius" : 23,
+        "weatherServiceName" : "Forecast.io",
+        "windBearing" : 117,
+        "sunriseDate" : "1970-01-01T00:00:00Z",
+        "conditionsDescription" : "Clear",
+        "pressureMB" : 1013.0399780273438,
+        "visibilityKM" : 10.010000228881836,
+        "relativeHumidity" : 78,
+        "windSpeedKPH" : 6.869999885559082,
+        "weatherCode" : "clear-night",
+        "sunsetDate" : "1970-01-01T00:00:00Z"
+      },
+      "timeZone" : "Asia\/Hong_Kong",
+      "uuid" : "32009FE4CAAA422380262B1E5AAF7200",
+      "duration" : 0,
+      "photos" : [
+        {
+          "fnumber" : "(null)",
+          "orderInEntry" : 0,
+          "width" : 2048,
+          "type" : "jpeg",
+          "identifier" : "2BEE3FA43830410ABE42DBCFCD753BA0",
+          "height" : 2048,
+          "md5" : "dc38cad03c7feeb2ff472af93182669c",
           "focalLength" : "(null)"
         }
       ]
