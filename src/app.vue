3abf2bd45519b10dc9e756cf74c5fa3d298520d8
zizy <mezizy@live.com>
2019-02-14 18:27:20 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="bg-light">
-    <div style="position: absolute;z-index:999"><img src="./assets/inwechat_wb.gif" alt=""></div>
+    <div style="position: absolute;z-index:999" :class="{'hidden':true}"><img src="./assets/inwechat_wb.gif" alt=""></div>
     <nav class="navbar navbar-expand navbar-light bg-light">
       <a class="navbar-brand" href="#">Zizy's Blog</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,6 +32,9 @@ export default {
 </script>
 
 <style>
+  .hidden{
+    display: none;
+  }
   .browsehappy {
     margin: 0.2em 0;
     background: #ccc;
